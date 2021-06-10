1778c77cc231d55f40f7e23913c257742b7b3f62
Yuppe-jp-eng <nakamura.yutaro78@gmail.com>
2021-06-10 12:43:26 +0900

    Googleログイン機能

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -12,7 +12,9 @@
             <h2 class="h3 card-title text-center mt-2">ログイン</h2>
 
             @include('errors.error_card')
-            
+            <a href="{{ route('login.{provider}', ['provider' => 'google']) }}" class="btn btn-block btn-danger">
+              <i class="fab fa-google mr-1"></i>Googleでログイン
+            </a>
             <div class="card-text">
               <form method="POST" action="{{ route('login') }}">
                 @csrf
