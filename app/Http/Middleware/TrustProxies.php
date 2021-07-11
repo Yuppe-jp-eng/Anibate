d4f2e21a632182b639e377e91304303c5be97dd2
Yuppe-jp-eng <nakamura.yutaro78@gmail.com>
2021-07-11 15:35:17 +0900

    log確認

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -15,7 +15,7 @@ class TrustProxies extends Middleware
     protected $proxies = '*';
 
     /**
-     * The headers that should be used to detect proxies.
+     * The headers that should be used to detect proxies
      *
      * @var int
      */
