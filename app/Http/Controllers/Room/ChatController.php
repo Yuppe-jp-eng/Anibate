a8bfe7e97609030437d8cd235c26188561ea81be
Yuppe-jp-eng <nakamura.yutaro78@gmail.com>
2021-10-19 11:22:31 +0900

    チャット機能のルート、コントローラー作成

---
+++ app/Http/Controllers/Room/ChatController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Room;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    public function show() {
+
+    }
+
+    public function store() {
+        
+    }
+}
