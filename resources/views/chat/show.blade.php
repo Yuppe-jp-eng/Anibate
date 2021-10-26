ea6257fd0261726d19e7a7ed56f93875fbc7d3c0
Yuppe-jp-eng <nakamura.yutaro78@gmail.com>
2021-10-26 13:55:27 +0900

    チャット画面の整形

--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -2,7 +2,7 @@
 @include('layouts.nav')
 @section('content')
     <div class="container mt-5 d-flex flex-column">
-      <h4>{{ $room->name }}</h4>
+      <h4>{{ $room->name }}<span>（{{ $room->users->count() }}）</span></h4>
       <chat
        :chat-messages='@json($chats)'
        auth-id='{{ Auth::id() }}'
